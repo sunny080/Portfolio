3263f27aeee342db541688007f1a20107d9aef85
Sunny <trajputtrana@gmail.com>
2022-04-06 11:16:28 +0500

    navbar miner fixes

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-let wrapper = document.querySelector('.link');
-let toggle = document.querySelector('.burger');
+// let wrapper = document.querySelector('.link');
+// let toggle = document.querySelector('.burger');
 
 
-window.onload=function(){
+// window.onclick=function(){
     
-    toggle.onclick=function(){
-    toggle.classList.toggle('active'); 
-    wrapper.classList.toggle('open')
-    }
+//     toggle.onclick=function(){
+//     toggle.classList.toggle('active'); 
+//     wrapper.classList.toggle('open')
+//     }
     
-}
+// }
+
+
+
+
+
+
+const nav = document.querySelector(".link")
+const burger = document.querySelector(".burger")
+const links = nav.querySelectorAll("a")
+
+burger.addEventListener("click",() => {
+    nav.classList.toggle("open");
+    burger.classList.toggle("active");
+});
+
+links.forEach(link => {
+    link.addEventListener('click',() => {
+        nav.classList.toggle("open");
+        burger.classList.toggle("active");
+
+    });
+});
